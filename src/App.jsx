ff2c8e68276f7bb9c0e4e4ea4bc0ec2aef6d0634
AlexIJuravlev <cawasimus@gmail.com>
2025-02-04 11:11:48 +0300

    completed the first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
 import './index.css';
 
 export const App = () => {
-	return (
-		<div className='App'>
-		</div>
+	const [todos, setTodos] = useState([])
 
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loaded) => loaded.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos)
+			})
+	}, []);
+
+	const todoListId = todos.filter((todo) => todo.userId === 1)
+	const userTodoList = todoListId.map(({ id, title, completed }) => (
+		<div className='todos' key={id}>
+			<div className='todos__task'>
+				<span className='todos__taskNumber'>{id}</span>
+				<span className='todos__title'> - {title}</span>
+				<input type='checkbox' checked={completed} />
+			</div>
+		</div>
+	));
+
+	return (
+		<>
+			<div className='main'>{userTodoList}</div>
+		</>
 	);
 };
